3126d45f87f695c94d74327c80fa7cb70244da62
Bruno Vinicius <118493267+bruuno1994@users.noreply.github.com>
2024-06-26 21:13:59 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -38,7 +38,6 @@ if __name__ == "__main__":
     limiar_distancia = 10
 
     pergunta = sys.argv[1]
-    print(f"Pergunta recebida: {pergunta}")
 
     resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
     print("Resposta:", resposta)
